9d3873c69b24d4a9a25957bcff6b3f4a823282f4
SATYAM <satyam.dev29@gmail.com>
2019-08-26 21:40:54 +0530

    Updated Blog UI

---
+++ build/precache-manifest.3be4a26841108d9ac0dc068ed72cfbde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c2de4c86b1692f3e0eec5533298856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98bd2ef2201b877a1794",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e1b748a9bd12167162ae",
+    "url": "/static/css/main.c9df1ed7.chunk.css"
+  },
+  {
+    "revision": "98bd2ef2201b877a1794",
+    "url": "/static/js/2.1e8b576d.chunk.js"
+  },
+  {
+    "revision": "e1b748a9bd12167162ae",
+    "url": "/static/js/main.d885e6d8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
